d648f89e572008cb0735c4b7735521d6bce8a11d
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2023-07-22 02:24:48 +0200

    Changed the whole project, the + method works properly

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 
-let numberArray = [2, 5, 10, 3];
-let dividableNumbers = [8, 4];
+// vars
+const numDisplay = document.querySelector('#display');
+
+let storedValue = 0;
+let operator = ''
+let selectedOperator = false;
+
+//check for clearing display
+function checkOperator() {
+    if (selectedOperator === true){
+        numDisplay.innerHTML = ' '
+        selectedOperator = false    
+    }
+}
 
 
-// basic math functions
-//all of the 4 basic functions passed the tests and are working properly
+//add numbers after the number button is clicked
+function addOne() {
+    checkOperator()
+    numDisplay.innerHTML = numDisplay.innerHTML + 1
+};
+function addTwo() {
+    checkOperator()
+    numDisplay.innerHTML = numDisplay.innerHTML + 2
+};
 
-//adding numbers
-function addNumbers(numbers) {return numbers.reduce((total, num) => total + num)};
 
-//substracting numbers
-function substractNumbers(numbers) {return numbers.reduce((total, num) => total - num)};
+//adds numbers
+function addPlus() {
+        // * if add method wasn't clicked, store the value thats currently
+        // * inside of display
+    if (operator !== '+') {
+    storedValue = numDisplay.innerHTML
+    } else {
+        // * else if method was clicked and is clicked again, store the 
+        // * previous stored value + current display value
+        storedValue = Number(storedValue) + Number(numDisplay.innerHTML)
+    }
+    operator = '+'
+    selectedOperator = true
+};
 
-//multiplying numbers
-function multiplyNumbers(numbers) {return numbers.reduce((total,num) => total * num)};
-
-//dividing numbers
-function divideNumbers(numbers) {return numbers.reduce((total, num) => total/num)};
-
+//does the operations
+function operate() {
+    if (operator === '+') {
+        console.log(Number(storedValue) + Number(numDisplay.innerHTML));
+        numDisplay.innerHTML = Number(storedValue) + Number(numDisplay.innerHTML)
+        operator = '';
+        selectedOperator = false  
+    } 
+};
